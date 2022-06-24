4c3bde558842592af76941ce6131238db234df32
asabya <asabya@gmail.com>
2022-06-24 10:53:47 +0530

    group replication tests

--- datahop/main.go
+++ datahop/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"os"
@@ -8,6 +9,7 @@ import (
 	"time"
 
 	"github.com/datahop/ipfs-lite/pkg"
+	"github.com/datahop/ipfs-lite/pkg/store"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/testground/sdk-go/network"
 	"github.com/testground/sdk-go/run"
@@ -20,6 +22,7 @@ var testCases = map[string]interface{}{
 	"private-connection": PrivateConnection,
 	"group-open":         GroupOpen,
 	"group-closed":       GroupClosed,
+	"group-replication":  GroupReplication,
 }
 
 func main() {
@@ -343,7 +346,7 @@ func GroupClosed(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
 		}
 		for _, addrInfo := range allPeers {
 			nemMemberPubKey := comm.Node.GetPubKey(addrInfo.ID)
-			
+
 			err = m.GroupAddMember(comm.Node.AddrInfo().ID, addrInfo.ID, groups[0].GroupID, comm.Node.GetPrivKey(), nemMemberPubKey)
 			if err != nil {
 				return err
@@ -360,5 +363,123 @@ func GroupClosed(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
 	if len(groups) > 0 {
 		runenv.RecordMessage("group id: %s, name: %s, owner: %s", groups[0].GroupID.String(), groups[0].Name, groups[0].OwnerID.String())
 	}
+	return nil
+}
+
+func GroupReplication(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
+	totalTime := time.Minute * 1
+	ctx, cancel := context.WithTimeout(context.Background(), totalTime)
+	defer cancel()
+	initCtx.MustWaitAllInstancesInitialized(ctx)
+	if !runenv.TestSidecar {
+		return fmt.Errorf("env does not support sidecar")
+	}
+	client := sync.MustBoundClient(ctx, runenv)
+	defer client.Close()
+	netclient := network.NewClient(client, runenv)
+	config := &network.Config{
+		Network: "default",
+		Enable:  true,
+		Default: network.LinkShape{
+			Latency:   100 * time.Millisecond,
+			Bandwidth: 1 << 20, // 1Mib
+		},
+		CallbackState: "network-configured",
+	}
+
+	err := netclient.ConfigureNetwork(ctx, config)
+	if err != nil {
+		return err
+	}
+	seq := initCtx.SyncClient.MustSignalAndWait(ctx, "ip-allocation", runenv.TestInstanceCount)
+	usr, err := user.Current()
+	absoluteRoot := usr.HomeDir + string(os.PathSeparator) + ".datahop"
+	err = pkg.Init(absoluteRoot, "4321")
+	if err != nil {
+		panic(err)
+	}
+	comm, err := pkg.New(context.Background(), absoluteRoot, "4321", nil)
+	if err != nil {
+		panic(err)
+	}
+	_, err = comm.Start("", false)
+	if err != nil {
+		panic(err)
+	}
+
+	initCtx.SyncClient.MustSignalAndWait(ctx, "listening", runenv.TestInstanceCount)
+
+	st := sync.NewTopic("transfer-addr", &peer.AddrInfo{})
+	_, err = client.Publish(ctx, st, comm.Node.AddrInfo())
+	if err != nil {
+		panic(err)
+	}
+	tch := make(chan *peer.AddrInfo, runenv.TestInstanceCount)
+	client.Subscribe(ctx, st, tch)
+
+	for i := 0; i < runenv.TestInstanceCount; i++ {
+		t := <-tch
+		if t.ID != comm.Node.AddrInfo().ID {
+			err = comm.Node.Connect(*t)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+
+	initCtx.SyncClient.MustSignalAndWait(ctx, "connected", runenv.TestInstanceCount)
+	m := comm.Node.ReplManager()
+	if seq == 1 {
+		gm, err := m.CreateOpenGroup("TestGroup", comm.Node.AddrInfo().ID, comm.Node.GetPrivKey())
+		if err != nil {
+			panic(err)
+		}
+		runenv.RecordMessage("Peer %s Created group: %s", comm.Node.AddrInfo().ID, gm.GroupID.String())
+
+		peers := comm.Node.Peers()
+		for _, peerID := range peers {
+			newPeerId, err := peer.Decode(peerID)
+			if err != nil {
+				return err
+			}
+			nemMemberPubKey := comm.Node.GetPubKey(newPeerId)
+			err = m.GroupAddMember(comm.Node.AddrInfo().ID, newPeerId, gm.GroupID, comm.Node.GetPrivKey(), nemMemberPubKey)
+			if err != nil {
+				return err
+			}
+		}
+
+		for i := 0; i < 10; i++ {
+			data := bytes.NewReader([]byte(fmt.Sprintf("%d", time.Now().Unix())))
+			info := &store.Info{
+				Tag:         fmt.Sprintf("%d", i),
+				Type:        "byte",
+				Name:        fmt.Sprintf("noname_%d", i),
+				IsEncrypted: false,
+				Size:        data.Size(),
+			}
+			hash, err := comm.Node.GroupAdd(ctx, data, info, gm.GroupID.String())
+			if err != nil {
+				return err
+			}
+			runenv.RecordMessage("content hash %s in group id: %s", hash, gm.GroupID.String())
+			<-time.After(time.Second)
+		}
+	}
+	initCtx.SyncClient.MustSignalAndWait(ctx, "content-distribution", runenv.TestInstanceCount)
+
+	<-time.After(time.Second * 5)
+	groups, err := m.GroupGetAllGroups(comm.Node.AddrInfo().ID, comm.Node.GetPrivKey())
+	if err != nil {
+		return err
+	}
+	if len(groups) < 0 {
+		return fmt.Errorf("peer is not a member of any group")
+	}
+	contents, err := m.GroupGetAllContent(comm.Node.AddrInfo().ID, groups[0].GroupID, comm.Node.GetPrivKey())
+	if err != nil {
+		return err
+	}
+	runenv.RecordMessage("group id: %s has %d number of content", groups[0].GroupID.String(), len(contents))
 	return nil
 }
